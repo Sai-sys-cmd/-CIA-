d6cb669ea12badb0eb4e42f17f0355e49b0608f7
Sai-sys-cmd <s300038602@ddsbstudent.ca>
2024-11-05 10:58:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const numberDisplay = document.getElementById("numberDisplay"); 
 const generateButton = document.getElementById("generateButton"); 
 
-// Add an event listener to the button
 generateButton.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * 100); 
     numberDisplay.textContent = randomNumber; 
